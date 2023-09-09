a1f393797cd791699612d4dded331770b80ec85e
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-09-09 00:03:18 -0300

    Riquisitos de consulta de extrato apenas de clientes cadastrado

--- 02-finapi/src/index.js
+++ 02-finapi/src/index.js
@@ -43,7 +43,7 @@ app.get("/statement/:cpf", (request, response) => {
     const customer = customers.find((customer) => customer.cpf === cpf);
 
     if(!customer){
-        return response.status(400).send({ error: 'Customer not found!'});
+        return response.status(400).json({ error: 'Customer not found!'});
     } 
 
     return response.json(customer.statement);
