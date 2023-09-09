1d62e5906a917710f39dfc54fc075e084bfa33db
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-09-09 18:25:53 -0300

    Implementado saque e saldo

--- 02-finapi/src/index.js
+++ 02-finapi/src/index.js
@@ -31,6 +31,18 @@ function verifyIfExistsAccountCPF(request, response, next){
     return next();
 }
 
+// Consulta saldo
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit'){
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+    return balance;
+}
+
 /**
  * cpf - string
  * name - string
@@ -69,6 +81,28 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
         amount,
         created_at: new Date(),
         type: 'credit'
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+// Efetuar saque
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance  < amount){
+        return response.status(400).json({ error: 'Insufficient funds!'});
+    }
+
+    const statementOperation = {
+        amount, 
+        created_at: new Date(),
+        type: 'debit'
     }
 
     customer.statement.push(statementOperation);
